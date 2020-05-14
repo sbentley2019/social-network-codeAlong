37a2a0d15f2c2168878484d49fa0b20d46cf94e9
sbentley2019 <sbentley000@gmail.com>
2020-05-14 18:26:04 -0400

    add formidable package for file upload and use it in post controller

--- controllers/post.js
+++ controllers/post.js
@@ -1,7 +1,10 @@
 const Post = require("../models/post");
+const formidable = require("formidable");
+const fs = require("fs");
 
 exports.getPosts = async (req, res) => {
   const posts = await Post.find()
+    .populate("postedBy", "_id name")
     .select("_id title body")
     .then((posts) => {
       res.json({ posts });
@@ -9,12 +12,30 @@ exports.getPosts = async (req, res) => {
     .catch((err) => console.log(err));
 };
 
-exports.createPost = async (req, res) => {
-  const post = new Post(req.body);
-  post
-    .save()
-    .then((result) => {
-      res.json({ post: result });
-    })
-    .catch((err) => console.log(err));
+exports.createPost = async (req, res, next) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Image could not be uploaded",
+      });
+    }
+    let post = new Post(fields);
+    req.user.hashed_password = undefined;
+    req.user.salt = undefined;
+    post.postedBy = req.user;
+    if (files.photo) {
+      post.photo.data = fs.readFileSync(files.photo.path);
+      post.photo.contentType = files.photo.type;
+    }
+    post.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: err,
+        });
+      }
+      res.json(result);
+    });
+  });
 };
--- routes/post.js
+++ routes/post.js
@@ -6,7 +6,12 @@ const { createPostValidator } = require("../validators");
 
 module.exports = () => {
   router.get("/", getPosts);
-  router.post("/post", requireSignin, createPostValidator, createPost);
+  router.post(
+    "/post/new/:userId",
+    requireSignin,
+    createPost,
+    createPostValidator
+  );
   router.param("userId", userById);
 
   return router;
