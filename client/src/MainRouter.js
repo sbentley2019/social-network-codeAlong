9837218bb921ea5b74647a1e3cffce1c4541a083
sbentley2019 <sbentley000@gmail.com>
2020-05-16 13:26:27 -0400

    add navigation bar, needs styling

--- client/src/MainRouter.js
+++ client/src/MainRouter.js
@@ -3,10 +3,12 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./core/Home";
 import Signup from "./user/Signup";
 import Login from "./user/Login";
+import Menu from "./core/Menu";
 
 export default function MainRouter() {
   return (
     <div>
+      <Menu />
       <Switch>
         <Route exact path="/" component={Home}></Route>
         <Route path="/signup" component={Signup}></Route>
