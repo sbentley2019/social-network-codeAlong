f93a0420f871444d9c725204b3ba08c44af2a0a9
sbentley2019 <sbentley000@gmail.com>
2020-05-26 14:30:16 -0400

    deploy to heroku

--- server.js
+++ server.js
@@ -54,4 +54,12 @@ app.use(function (err, req, res, next) {
   }
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
