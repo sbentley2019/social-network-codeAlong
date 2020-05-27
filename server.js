0dcd60017cb637de4aaca6fa46c960b2f0582f68
sbentley2019 <sbentley000@gmail.com>
2020-05-27 08:45:31 -0400

    test api docs route

--- server.js
+++ server.js
@@ -27,23 +27,23 @@ const postRoutes = require("./routes/post");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 
-// app.get("/", (req, res) => {
-//   fs.readFile("./docs/apiDocs.json", (err, data) => {
-//     if (err) {
-//       return res.status(400).json({
-//         error: err,
-//       });
-//     }
-//     const docs = JSON.parse(data);
-//     res.json(docs);
-//   });
-// });
-
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
 app.use(cors());
+
+app.get("/", (req, res) => {
+  fs.readFile("./docs/apiDocs.json", (err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    const docs = JSON.parse(data);
+    res.json(docs);
+  });
+});
 
 app.use("/post", postRoutes());
 app.use("/auth", authRoutes());
