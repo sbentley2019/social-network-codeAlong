7e483b906be034618241e646f3f72efc0a7d1094
sbentley2019 <sbentley000@gmail.com>
2020-05-29 18:09:29 -0400

    add lists for followers and following on profile page

--- client/src/user/ProfileTabs.js
+++ client/src/user/ProfileTabs.js
@@ -4,72 +4,132 @@ import user_avatar from "../images/user_avatar.png";
 
 export default function ProfileTabs(props) {
   return (
-    <>
-      <div>
-        <h3 className="text-primary">Followers</h3>
-        <hr />
-        <ul
-          style={{
-            display: "flex",
-            flexWrap: "wrap",
-            listStyle: "none",
-            paddingLeft: "0",
-          }}
-        >
+    <div>
+      <div className="row">
+        <div className="col-md-4">
+          <h3 className="text-primary">Followers</h3>
+          <hr />
           {props.followers.map((person, i) => {
             return (
-              <li key={i} className="m-5">
+              <div key={i} className="row">
                 <Link to={`/user/${person._id}`}>
-                  <figure
-                    style={{
-                      height: "200px",
-                      width: "200px",
-                    }}
-                  >
-                    <img
-                      src={`http://localhost:3001/user/photo/${person._id}`}
-                      alt={person.name}
-                      className="img-thumbnail"
-                      onError={(e) => (e.target.src = user_avatar)}
-                    />
-                    <figcaption>{person.name}</figcaption>
-                  </figure>
+                  <img
+                    className="float-left mr-2"
+                    height="50px"
+                    src={`http://localhost:3001/user/photo/${person._id}`}
+                    onError={(e) => (e.target.src = user_avatar)}
+                    alt={person.name}
+                  />
+                  <div>
+                    <h3>{person.name}</h3>
+                  </div>
                 </Link>
-              </li>
+                <p style={{ clear: "both", textAlign: "center" }}>
+                  {person.about}
+                </p>
+              </div>
             );
           })}
-        </ul>
-      </div>
-      <div>
-        <h3 className="text-primary">Following</h3>
-        <hr />
-        <ul
-          style={{
-            display: "flex",
-            flexWrap: "wrap",
-            listStyle: "none",
-            paddingLeft: "0",
-          }}
-        >
+        </div>
+
+        <div className="col-md-4">
+          <h3 className="text-primary">Following</h3>
+          <hr />
           {props.following.map((person, i) => {
             return (
-              <li key={i} className="m-5">
+              <div key={i} className="row">
                 <Link to={`/user/${person._id}`}>
-                  <figure style={{ height: "200px", width: "200px" }}>
-                    <img
-                      src={`http://localhost:3001/user/photo/${person._id}`}
-                      alt={person.name}
-                      className="img-thumbnail"
-                      onError={(e) => (e.target.src = user_avatar)}
-                    />
-                    <figcaption>{person.name}</figcaption>
-                  </figure>
+                  <img
+                    className="float-left mr-2"
+                    height="50px"
+                    src={`http://localhost:3001/user/photo/${person._id}`}
+                    onError={(e) => (e.target.src = user_avatar)}
+                    alt={person.name}
+                  />
+                  <div>
+                    <h3>{person.name}</h3>
+                  </div>
                 </Link>
-              </li>
+                <p style={{ clear: "both", textAlign: "center" }}>
+                  {person.about}
+                </p>
+              </div>
             );
           })}
-        </ul>
+        </div>
+        <div className="col-md-4">
+          <h3 className="text-primary">Posts</h3>
+        </div>
       </div>
-    </>
+    </div>
   );
+  // return (
+  //   <>
+  //     <div>
+  //       <h3 className="text-primary">Followers</h3>
+  //       <hr />
+  //       <ul
+  //         style={{
+  //           display: "flex",
+  //           flexWrap: "wrap",
+  //           listStyle: "none",
+  //           paddingLeft: "0",
+  //         }}
+  //       >
+  //         {props.followers.map((person, i) => {
+  //           return (
+  //             <li key={i} className="m-5">
+  //               <Link to={`/user/${person._id}`}>
+  //                 <figure
+  //                   style={{
+  //                     height: "200px",
+  //                     width: "200px",
+  //                   }}
+  //                 >
+  //                   <img
+  //                     src={`http://localhost:3001/user/photo/${person._id}`}
+  //                     alt={person.name}
+  //                     className="img-thumbnail"
+  //                     onError={(e) => (e.target.src = user_avatar)}
+  //                   />
+  //                   <figcaption>{person.name}</figcaption>
+  //                 </figure>
+  //               </Link>
+  //             </li>
+  //           );
+  //         })}
+  //       </ul>
+  //     </div>
+  //     <div>
+  //       <h3 className="text-primary">Following</h3>
+  //       <hr />
+  //       <ul
+  //         style={{
+  //           display: "flex",
+  //           flexWrap: "wrap",
+  //           listStyle: "none",
+  //           paddingLeft: "0",
+  //         }}
+  //       >
+  //         {props.following.map((person, i) => {
+  //           return (
+  //             <li key={i} className="m-5">
+  //               <Link to={`/user/${person._id}`}>
+  //                 <figure style={{ height: "200px", width: "200px" }}>
+  //                   <img
+  //                     src={`http://localhost:3001/user/photo/${person._id}`}
+  //                     alt={person.name}
+  //                     className="img-thumbnail"
+  //                     onError={(e) => (e.target.src = user_avatar)}
+  //                   />
+  //                   <figcaption>{person.name}</figcaption>
+  //                 </figure>
+  //               </Link>
+  //             </li>
+  //           );
+  //         })}
+  //       </ul>
+  //     </div>
+  //   </>
+  // );
 }
